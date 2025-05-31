4bfcda6be83378718c13aa4794748c3dcc0f515f
md <marcindomurad@gmail.com>
2025-05-31 14:29:20 +0200

    feat: add optional UserId property to LicenseGenerationRequest DTO
    This resolves issues with foreign key exception on license save with
    empty UserId property in e.g. postgres database.

--- src/Aegis.Server/DTOs/LicenseGenerationRequest.cs
+++ src/Aegis.Server/DTOs/LicenseGenerationRequest.cs
@@ -5,13 +5,13 @@ namespace Aegis.Server.DTOs;
 
 public class LicenseGenerationRequest
 {
-    public LicenseType LicenseType { get; set; }
-    public DateTime? ExpirationDate { get; set; }
-    public Guid ProductId { get; init; }
-    public string IssuedTo { get; init; } = string.Empty;
-    public int? MaxActiveUsersCount { get; }
-    public string? HardwareId { get; }
-    public TimeSpan? SubscriptionDuration { get; }
-    public Dictionary<Guid, Feature> Features { get; init; } = [];
-
+  public LicenseType LicenseType { get; set; }
+  public DateTime? ExpirationDate { get; set; }
+  public Guid ProductId { get; init; }
+  public string IssuedTo { get; init; } = string.Empty;
+  public int? MaxActiveUsersCount { get; }
+  public string? HardwareId { get; }
+  public TimeSpan? SubscriptionDuration { get; }
+  public Dictionary<Guid, Feature> Features { get; init; } = [];
+  public Guid? UserId { get; init; }
 }
